03360da40988a727d0d289e8ca7ef3b03f3ab039
unknown <Shivam>
2022-02-08 23:43:31 +0530

    First js

---
+++ index.js
@@ -0,0 +1,22 @@
+var a;
+
+a=10;
+console.log(typeof a);
+console.log(a);
+
+a="hello";
+console.log(a);
+console.log(typeof a);
+
+a = true;
+console.log(a);
+console.log(typeof a);
+
+var str = 'a';
+console.log(typeof a);
+str = " how are you?\nI an fine";
+console.log(str);
+
+var b = `hi hope you are do well
+ and it's good`
+ console.log(b);
