48f06228b917128f189ea04b4aa6af168a77af62
unknown <Shivam>
2022-02-12 22:11:10 +0530

    js first

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ var str = 'a';
 console.log(typeof a);
 str = " how are you?\nI an fine";
 console.log(str);
-
 var b = `hi hope you are do well
  and it's good`
  console.log(b);
+
+//  loop
+var num = 10;
+for( var i=0; i<num; i++){
+    if(i%2 == 0){
+        console.log(i);
+    }
+  
+}
+
